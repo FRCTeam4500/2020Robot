21fee3de5212576519e06dc071107095504cd243
Personator11 <tbutler16@claytonschools.net>
2020-01-14 17:14:50 -0600

    removed some stuff that doesnt work and fixed other

--- src/main/java/frc/robot/components/dashboard/DashboardDecorator.java
+++ src/main/java/frc/robot/components/dashboard/DashboardDecorator.java
@@ -7,7 +7,8 @@
 
 package frc.robot.components.dashboard;
 
-import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 
 /**
  * The base decorator for all dashboard decorator components. By extending this class, a decorator
@@ -15,7 +16,7 @@ import edu.wpi.first.wpilibj.SendableBase;
  * {@link edu.wpi.first.wpilibj.Sendable Sendable}, such Shuffleboard. A decorator is intended to
  * wrap around a component and passed around instead of the component itself.
  */
-public abstract class DashboardDecorator extends SendableBase {
+public abstract class DashboardDecorator implements Sendable {
 
     /**
      * Creates a decorator with the respective parameters. In Shuffleboard, the decorator is defined
@@ -26,7 +27,7 @@ public abstract class DashboardDecorator extends SendableBase {
      * @param subsystem the subsystem which the component is part of
      */
     public DashboardDecorator(String name, String subsystem) {
-        setName(name);
-        setSubsystem(subsystem);
+        SendableRegistry.setName(this, name);
+        SendableRegistry.setSubsystem(this, subsystem);
     }
 }
