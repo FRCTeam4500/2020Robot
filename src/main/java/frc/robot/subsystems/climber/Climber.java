1dad0af786b626772f24e1cad353ab20e12b65a4
Personator11 <tbutler16@claytonschools.net>
2020-02-04 16:15:04 -0600

    Climber stuff

---
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.climber;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.components.ISpeedSetterComponent;
+
+public class Climber extends SubsystemBase {
+    ISpeedSetterComponent motor1;
+    ISpeedSetterComponent motor2;
+    public Climber(ISpeedSetterComponent motor1, ISpeedSetterComponent motor2) {
+
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    }
+
+    public void setSpeed(double speed){
+        motor1.setSpeed(speed);
+        motor2.setSpeed(speed);
+    }
+
+}
+
---
+++ src/main/java/frc/robot/subsystems/climber/IClimberOI.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.climber;
+
+public interface IClimberOI {
+    public double getClimberHeight();
+}
