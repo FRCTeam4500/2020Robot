c1a263149e92639cc10fce307154a6fd2998b980
Lolzkarry <lqian11@claytonschools.net>
2020-02-07 17:07:14 -0600

    set up network tables for sensors

--- src/main/java/frc/robot/subsystems/indexer/command/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/command/Indexer.java
@@ -11,8 +11,22 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.components.ISpeedSetterComponent;
 import frc.robot.components.Sensor;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Indexer extends CommandBase {
+  private NetworkTableInstance nt;
+  private NetworkTable table;
+  private NetworkTableEntry Sensor0;
+  private NetworkTableEntry Sensor1;
+  private NetworkTableEntry Sensor2;
+
+  private NetworkTableEntry Sensor3;
+  private NetworkTableEntry Sensor4;
+  private NetworkTableEntry Sensor5;
+
+
   ISpeedSetterComponent indexMotor = new ISpeedSetterComponent(){
   
     @Override
@@ -27,6 +41,7 @@ public class Indexer extends CommandBase {
       SmartDashboard.putBoolean("Run Intake Motor", speed != 0);
     }
   };
+  Sensor sensor0 = new Sensor();
   Sensor sensor1 = new Sensor();
   boolean indexingBall = false;
   int ballCount = 0;
@@ -34,15 +49,41 @@ public class Indexer extends CommandBase {
   Sensor sensor3 = new Sensor();
   Sensor sensor4 = new Sensor();
   Sensor sensor5 = new Sensor();
-  Sensor sensor6 = new Sensor();
+
 
   
 
   /**
    * Creates a new Indexer.
    */
-  public Indexer() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  public Indexer(NetworkTable table) {
+    nt = NetworkTableInstance.getDefault();
+    table = nt.getTable("SmartDashboard");
+    Sensor0 = table.getEntry("Sensor0");
+    Sensor1 = table.getEntry("Sensor1");
+    Sensor2 = table.getEntry("Sensor2");
+    Sensor3 = table.getEntry("Sensor3");
+    Sensor4 = table.getEntry("Sensor4");
+    Sensor5 = table.getEntry("Sensor5");
+  }
+  
+  public double getSensor0() {
+    return Sensor0.getDouble(0);
+  }
+  public double getSensor1() {
+    return Sensor1.getDouble(0);
+  }
+  public double getSensor2() {
+    return Sensor2.getDouble(0);
+  }
+  public double getSensor3() {
+    return Sensor3.getDouble(0);
+  }
+  public double getSensor4() {
+    return Sensor4.getDouble(0);
+  }
+  public double getSensor5() {
+    return Sensor5.getDouble(0);
   }
 
   // Called when the command is initially scheduled.
@@ -53,28 +94,28 @@ public class Indexer extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(sensor1.registersBall()){
+    if(sensor0.registersBall()){
       indexMotor.setSpeed(1);
       intakeMotor.setSpeed(0);
       indexingBall = true;
-      if(!sensor2.registersBall()) {
-          if(sensor2.registersBall()) {
+      if(!sensor1.registersBall()) {
+          if(sensor1.registersBall()) {
             indexMotor.setSpeed(0);
           }
       }
+      if(!sensor2.registersBall()) {
+        if(sensor2.registersBall()) {
+          indexMotor.setSpeed(0);
+        }
+      }
+
       if(!sensor3.registersBall()) {
         if(sensor3.registersBall()) {
           indexMotor.setSpeed(0);
         }
       }
-
       if(!sensor4.registersBall()) {
         if(sensor4.registersBall()) {
-          indexMotor.setSpeed(0);
-        }
-      }
-      if(!sensor5.registersBall()) {
-        if(sensor5.registersBall()) {
           indexMotor.setSpeed(0);
         }
       }
