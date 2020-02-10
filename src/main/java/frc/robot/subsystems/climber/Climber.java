9d6979becc094b563696a3f202e1bd52bc02d60e
Personator11 <tbutler16@claytonschools.net>
2020-02-10 15:52:08 -0600

    changed climber motors to angle setters

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -2,23 +2,19 @@ package frc.robot.subsystems.climber;
 
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.components.ISpeedSetterComponent;
+import frc.robot.components.IAngleSetterComponent;
 
 public class Climber extends SubsystemBase {
-    ISpeedSetterComponent motor1;
-    ISpeedSetterComponent motor2;
-    public Climber(ISpeedSetterComponent motor1, ISpeedSetterComponent motor2) {
+    IAngleSetterComponent motor1;
+    IAngleSetterComponent motor2;
+    public Climber(IAngleSetterComponent motor1, IAngleSetterComponent motor2) {
         this.motor1 = motor1;
         this.motor2 = motor2;
-        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
-        //       in the constructor or in the robot coordination class, such as RobotContainer.
-        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
-        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
     }
 
-    public void setSpeed(double speed){
-        motor1.setSpeed(speed);
-        motor2.setSpeed(speed);
+    public void setSpeed(double angle){
+        motor1.setAngle(angle);
+        motor2.setAngle(angle);
     }
 
 }
