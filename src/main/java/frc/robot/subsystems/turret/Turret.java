fc4aedd0efa5426814b1092c978da30cf3d84537
Personator11 <tbutler16@claytonschools.net>
2020-02-17 11:10:27 -0600

    delet useless garbage

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.turret;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.components.IAngleSetterComponent;
-import frc.robot.components.ISmartMotorComponent;
 
 public class Turret extends SubsystemBase {
     private IAngleSetterComponent turnMotor;
