2cae93af97543534c917faaf8924b6a8918baac1
Lolzkarry <lqian11@claytonschools.net>
2020-02-20 16:13:36 -0600

    a

--- src/main/java/frc/robot/subsystems/turret/factory/DefaultTurretFactory.java
+++ src/main/java/frc/robot/subsystems/turret/factory/DefaultTurretFactory.java
@@ -8,10 +8,10 @@ public class DefaultTurretFactory implements ITurretFactory{
     public Turret makeTurret(){
 
         TalonSRXComponent turnMotor = new TalonSRXComponent(TurretMap.TURRET_MOTOR_PORT);
-        turnMotor.configForwardSoftLimitThreshold(9176, 0);
-        turnMotor.configReverseSoftLimitThreshold(-7646, 0);
-        turnMotor.configForwardSoftLimitEnable(true, 0);
-        turnMotor.configReverseSoftLimitEnable(true, 0);
+        turnMotor.configForwardSoftLimitThreshold(9176);
+        turnMotor.configReverseSoftLimitThreshold(-7646);
+        turnMotor.configForwardSoftLimitEnable(true);
+        turnMotor.configReverseSoftLimitEnable(true);
         return new Turret(turnMotor);
         
     }
