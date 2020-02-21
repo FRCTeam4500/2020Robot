0e0522dbebdd48eafee47d23f873c291149328b9
Personator11 <tbutler16@claytonschools.net>
2020-02-21 11:46:22 -0600

    document

--- src/main/java/frc/robot/components/IAngleGetterSetterComponent.java
+++ src/main/java/frc/robot/components/IAngleGetterSetterComponent.java
@@ -1,4 +1,8 @@
 package frc.robot.components;
 
+/**
+ * An interface that integrates IAngleGetterComponent and IAngleSetterComponent
+ */
+
 public interface IAngleGetterSetterComponent extends IAngleGetterComponent, IAngleSetterComponent {
 }
