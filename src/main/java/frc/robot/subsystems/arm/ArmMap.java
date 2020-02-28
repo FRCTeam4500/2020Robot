f5d58d64d81e0bfe1ded5a6492cc1b3dc70e28dd
Personator11 <tbutler16@claytonschools.net>
2020-02-28 15:35:11 -0600

    aerm

--- src/main/java/frc/robot/subsystems/arm/ArmMap.java
+++ src/main/java/frc/robot/subsystems/arm/ArmMap.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.arm;
 
 public class ArmMap {
-    public static final int ARM_MOTOR_PORT = 8;
+    public static final int ARM_MOTOR_PORT = 0;
+    public static final double ARM_MIN_VALUE = 0.0;
+    public static final double ARM_MAX_VALUE = -1.3805827091;
 }
