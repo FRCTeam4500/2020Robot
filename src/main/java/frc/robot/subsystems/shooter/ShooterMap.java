039c005ad5a52f087f3d451db620c2fe9a943a47
Personator11 <tbutler16@claytonschools.net>
2020-02-28 16:14:55 -0600

    no

--- src/main/java/frc/robot/subsystems/shooter/ShooterMap.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterMap.java
@@ -2,13 +2,10 @@ package frc.robot.subsystems.shooter;
 
 public class ShooterMap {
     //TODO: Make these correspond to their actual ports
-<<<<<<< Updated upstream
     public static final int TOP_MOTOR_PORT = 14;
     public static final int BOTTOM_MOTOR_PORT = 13;
-=======
     public static final int TOP_SPEED = -3300;
     public static final int BOTTOM_SPEED = -2800;
     public static final double COEFFICIENT = 0.8;
->>>>>>> Stashed changes
 
 }
