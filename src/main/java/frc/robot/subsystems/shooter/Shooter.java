361d28136e7b41b8a85714c315a1bd31e22bf194
Personator11 <tbutler16@claytonschools.net>
2020-02-29 13:49:25 -0600

    aetsped

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -16,5 +16,15 @@ public class Shooter extends SubsystemBase {
         this.topMotor.setAngularVelocity(topSpeed / 60 * 2 * Math.PI);
         this.bottomMotor.setAngularVelocity(bottomSpeed / 60 * 2 * Math.PI);
     }
+
+    public boolean atSpeeds(double threshold){
+        return Math.abs(desiredTopSpeed - radPerSecToRotPerMin(topMotor.getAngularVelocity())) < threshold && Math.abs(desiredBottomSpeed - radPerSecToRotPerMin(bottomMotor.getAngularVelocity())) < threshold;
+    }
+    public double radPerSecToRotPerMin(double radPerSec){
+        return radPerSec / Math.PI / 2.0 * 60;
+    }
+    public double rotPerMinToRadPerSec(double rotPerMin){
+        return rotPerMin / 60 * 2 * Math.PI;
+    }
 }
 
