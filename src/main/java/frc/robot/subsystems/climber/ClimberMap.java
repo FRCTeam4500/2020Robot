82baf1814fe06e68742f49b2c0dfb5f8d5feb670
Personator11 <tbutler16@claytonschools.net>
2020-03-02 16:24:56 -0600

    eleg

--- src/main/java/frc/robot/subsystems/climber/ClimberMap.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberMap.java
@@ -2,4 +2,6 @@ package frc.robot.subsystems.climber;
 
 public class ClimberMap {
     public static final int CLIMBER_MOTOR_PORT = 9;
+    public static final float MAX_MOTOR_VALUE = 0;
+    public static final float MIN_MOTOR_VALUE = 565;
 }
