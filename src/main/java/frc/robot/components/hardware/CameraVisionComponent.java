a526505fbd526a69fdb9f16057bdb59cf20781f5
Personator11 <tbutler16@claytonschools.net>
2020-03-05 17:39:59 -0600

    Revert "e"
    This reverts commit 6b4e710472b2d480a6c28f8dd9b435d64d830a99.

--- src/main/java/frc/robot/components/hardware/CameraVisionComponent.java
+++ src/main/java/frc/robot/components/hardware/CameraVisionComponent.java
@@ -5,7 +5,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.components.IVisionComponent;
 
-public class CameraVisionComponent {
+public class CameraVisionComponent implements IVisionComponent {
     /**
      * table values
      * hasValidTargets
@@ -27,10 +27,31 @@ public class CameraVisionComponent {
         return Math.toRadians(getEntry("diffY"));
     }
 
-    public double getAngleX(){
-        return getEntry("ax");
+    public double getTargetArea(){
+        return 0;
     }
 
+    public double getSkew(){
+        return 0;
+    }
+
+    public void setCameraMode(CameraMode mode){
+
+    }
+
+    public void setPipeline(int pipeline){
+        
+    }
+
+    public boolean hasValidTargets(){
+        var value = getEntry("hasValidTargets");
+        if (value == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     private double getEntry(String key) {
         //return table.getEntry(key).getDouble(0);
         return SmartDashboard.getNumber(key, 0);
