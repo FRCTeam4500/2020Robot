8734aef837431f6967eceaa96474d23d43614c10
Octillerysnacker <octillerysnacker@gmail.com>
2020-03-06 19:41:19 -0600

    lims

--- src/main/java/frc/robot/subsystems/climber/ClimberMap.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberMap.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems.climber;
 
 public class ClimberMap {
     public static final int CLIMBER_MOTOR_PORT = 15;
-    public static final float MAX_MOTOR_VALUE = -10;
-    public static final float MIN_MOTOR_VALUE = -550;
+    public static final float MAX_MOTOR_VALUE = 0;
+    public static final float MIN_MOTOR_VALUE = -318;
     public static final int SERVO_MOTOR_PORT = 0;
     public static final int SERVO_MAX_DEGREES = 92;
     public static final int SERVO_MIN_DEGREES = 85;
